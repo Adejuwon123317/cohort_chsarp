dfef42cb20da10d885195e2df48f90440d9e82e5
Olanrewaju Adejuwon <olanrewajuadejuwon4@gmail.com>
2025-09-10 18:16:58 -0700

    exception and attributes

---
+++ cohort-csharp/Information.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cohort_csharp
+{ 
+    //xml documentation
+    /// <summary>
+    /// for getting info about users
+    /// </summary>
+    public class Information
+    {
+        /// <summary>
+        /// this method is for confirming age 
+        /// </summary>
+        /// <param name="Age"></param>
+        /// <exception cref="Exception"></exception>
+        public static void ConfirmAge(int Age)
+        {
+            if(Age < 18)
+            {
+                throw new Exception("Access Denied must be at least 18 years old");
+            }
+            else
+            {
+                Console.WriteLine("Access granted you are welcome to the fun party!");
+            }
+
+        }
+        [Obsolete("this method is depricated use new one instead")]
+        public static void OldMethod()
+        {
+            Console.WriteLine("this is an old method");
+        }
+    }
+}
--- cohort-csharp/Program.cs
+++ cohort-csharp/Program.cs
@@ -4,38 +4,27 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Security;
 using System.Threading.Channels;
-List<Student> students = new List<Student>();
-students.Add(new Student() { Id = 01, Name = "Juwon", Grade = 90, Age = 21, Class = "400level" });
-students.Add(new Student() { Id = 02, Name = "Nathaniel", Grade = 65, Age = 19, Class = "200level" });
-students.Add(new Student() { Id = 03, Name = "Gabriel", Grade = 70, Age = 20, Class = "300level" });
-students.Add(new Student() { Id = 04, Name = "Isreal", Grade = 78, Age = 15, Class = "300level" });
-students.Add(new Student() { Id = 05, Name = "Joseph", Grade = 74, Age = 17, Class = "100level" });
-students.Add(new Student() { Id = 06, Name = "Tomiwa", Grade = 80, Age = 22, Class = "400level" });
-students.Add(new Student() { Id = 07, Name = "Enoch", Grade = 75, Age = 21, Class = "400level" });
-students.Add(new Student() { Id = 08, Name = "David", Grade = 65, Age = 21, Class = "300level" });
-students.Add(new Student() { Id = 09, Name = "Praise", Grade = 55, Age = 13, Class = "100level" });
-students.Add(new Student() { Id = 10, Name = "Peter", Grade = 77, Age = 18, Class = "400level" });
 
-ispromoted word = new ispromoted(qualify);
+//try
+//catch
+//finally
+//throw
+//try
+//{
+//    int[] values = { 7, 8, 6, 2 };
+//    Console.WriteLine(values[8]);
 
-Student.PromoteStudent(students, word);
 
-static bool qualify(Student students)
-{
-    if (students.Grade >= 75)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+//finally
+//{
+//    int x = 6;
+//    Console.WriteLine(x);
+//}
+Information.ConfirmAge(19);
+Console.ReadKey();
 
-//Console.WriteLine("Write a greeting message to send to your friend in uppercase");
-//string word = Console.ReadLine();
-//var result = word.ToLower();
-//Console.WriteLine(result);
-
-//Console.ReadKey();
-//
