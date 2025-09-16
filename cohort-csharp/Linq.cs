98e71409015efe09c89847baedd908951b5efda8
Olanrewaju Adejuwon <olanrewajuadejuwon4@gmail.com>
2025-09-16 15:43:22 -0700

    linq

---
+++ cohort-csharp/Linq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cohort_csharp
+{
+    //Language integrated query
+    //Query synthax or query expression synthax
+    //Method synthax or method expression synthax or fluent synthax
+    public class Linq
+    {
+    }
+    public class filtering
+    {
+        //where
+        //of type
+        List<int> intlist = new List<int> { 2, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+        public void WhereValue()
+        {
+            //method syntax
+            var filterdata = intlist.Where(x => x > 5);
+            foreach (var filter in filterdata)
+            {
+                Console.WriteLine(filter);
+            }
+        }
+    }
+}
--- cohort-csharp/Program.cs
+++ cohort-csharp/Program.cs
@@ -5,96 +5,38 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Security;
 using System.Threading.Channels;
+//linq methodsg
 
-//Queue
-//FIFO style(first in first out)
-//Queue<int>DataId = new Queue<int>();
-//DataId.Enqueue(102);
-//DataId.Enqueue(103);
-//DataId.Enqueue(104);
-//DataId.Enqueue(105);
-//DataId.Enqueue(106);
-//DataId.Enqueue(107);
-//DataId.Enqueue(108);
-//DataId.Enqueue(109);
-
-////foreach(var item in DataId)
-////{
-////    Console.WriteLine(item);
-////}
-
-//Console.WriteLine("Total Data :{0}", DataId.Count());
-
-////while (DataId.Count() > 0)
-////    Console.WriteLine(DataId.Dequeue());
-
-//if(DataId.Count() > 0)
-//{
-//    Console.WriteLine(DataId.Dequeue());
-//}
-
-//Console.WriteLine("Total Data :{0}", DataId.Count());
-
-
-//Stack
-//LIFO Last in first out
-//Stack<string> values = new Stack<string>();
-//values.Push("Corolla");
-//values.Push("Mercedes");
-//values.Push("Volkswagon");
-//values.Push("Toyota");
-//values.Push("Audi");
-//values.Push("Camry");
-
-////foreach (var item in values)
-////{
-////    Console.WriteLine(item);
-////}
-//Console.WriteLine("Total Data :{0}", values.Count());
-//if (values.Count() > 0)
-//{
-//    Console.WriteLine(values.Pop());
-//}
-//Console.WriteLine("Total Data :{0}", values.Count());
-Dictionary.MyMethod();
+//filtering
+//projecting 
+//    ordering
+//    joining
+//    grouping
+//    set operators
+//    element operators
+//    aggreegate operators
+//    quantifiers
+    //conversion methods
+IList<String> wordlist = new List<string>()
+{
+    "c# course",
+    "c++ Tutorial",
+    "java course",
+    "python course",
+    "swift course",
+    "Flutter Tutorial",
+    "kotlin Tutorial"
+};
+//query synthax
+var result = from s in wordlist
+             where s.Contains("Tutorial")
+             select s;
+foreach (var word in result)
+    Console.WriteLine(word);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//IList<String> wordlist = new List<string>()
-//{
-//    "c# course",
-//    "c++ Tutorial",
-//    "java course",
-//    "python course",
-//    "swift course",
-//    "Flutter Tutorial",
-//    "kotlin Tutorial"
-//};
-//var result = from s in wordlist
-//             where s.Contains("Tutorial")
-//             select s;
-//foreach (var word in result)
-//    Console.WriteLine(word);
-
-
-//filtering Filtering = new filtering();
-//Filtering.WhereValue();
-//SetOperatorClass.SelectSetOpaerator();
+filtering Filtering = new filtering();
+Filtering.WhereValue();
+Projection.SelectValue();
+////Projection.SelectSetOpaerator();
 //ElementOperator.ElementMethod();
