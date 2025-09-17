18f05d1ddf1d8f044517f8e7f4e8fb8f46959142
Olanrewaju Adejuwon <olanrewajuadejuwon4@gmail.com>
2025-09-16 20:21:44 -0700

    assignment

---
+++ cohort-csharp/AggregateAss.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace cohort_csharp
+{
+    public class AggregateAss
+    {
+        /// <summary>
+        /// A Method To Display All Aggregate Methods.
+        /// </summary>
+        public static void aggregate()
+        {
+            double[] element = { 2.0, 3.0, 5.0, 4.0, 7, 8, 12, 16 };
+
+            var count = element.Count(j => j % 2 == 0);
+            Console.WriteLine($"\tTotal_Even_Count: {count}\n");
+            var sum = element.Sum();
+            Console.WriteLine($"\tTotal_Sum: {sum}\n");
+            var max = element.Max();
+            Console.WriteLine($"\tMax_Value: {max}\n");
+            var min = element.Min();
+            Console.WriteLine($"\tMin_Value: {min}\n");
+            var average = element.Average();
+            Console.WriteLine($"\tAverage_Value: {average}\n");
+        }
+        
+
+    }
+}
--- cohort-csharp/Program.cs
+++ cohort-csharp/Program.cs
@@ -1,12 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using cohort_csharp;
-using System.Collections;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Net.Security;
-using System.Threading.Channels;
 
-SetOperators.SelectSetOperators();
-ElementOperator.ElementMethod();
-QuantifierOperatorClass.AnyMethod();
-aggregate.aggregatemethod();
+using cohort_csharp;
+
+//WordDictionary.MyMethod();
+//AggregateAss.aggregate();
+ManU_Football.Value();
+Console.ReadKey();
