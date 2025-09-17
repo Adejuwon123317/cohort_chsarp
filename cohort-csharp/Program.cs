de2f04387433bf17e693f8fa72e4ae4049e55226
Olanrewaju Adejuwon <olanrewajuadejuwon4@gmail.com>
2025-09-16 17:01:30 -0700

    2

--- cohort-csharp/Program.cs
+++ cohort-csharp/Program.cs
@@ -5,38 +5,8 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Security;
 using System.Threading.Channels;
-//linq methodsg
 
-//filtering
-//projecting 
-//    ordering
-//    joining
-//    grouping
-//    set operators
-//    element operators
-//    aggreegate operators
-//    quantifiers
-    //conversion methods
-IList<String> wordlist = new List<string>()
-{
-    "c# course",
-    "c++ Tutorial",
-    "java course",
-    "python course",
-    "swift course",
-    "Flutter Tutorial",
-    "kotlin Tutorial"
-};
-//query synthax
-var result = from s in wordlist
-             where s.Contains("Tutorial")
-             select s;
-foreach (var word in result)
-    Console.WriteLine(word);
-
-
-filtering Filtering = new filtering();
-Filtering.WhereValue();
-Projection.SelectValue();
-////Projection.SelectSetOpaerator();
-//ElementOperator.ElementMethod();
+SetOperators.SelectSetOperators();
+ElementOperator.ElementMethod();
+QuantifierOperatorClass.AnyMethod();
+aggregate.aggregatemethod();
